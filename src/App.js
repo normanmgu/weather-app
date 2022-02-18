f54c42180d838d8daee766bc861677686f12aec9
norman_mgu <norman.mgu@gmail.com>
2022-02-18 11:39:27 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+async function GetTemp() {
+  const APIKEY="c570c325ef0e117472d3a16d662290a8";
+  const long = "38.44";
+  const lat = "94.04";
+  let latitude;
+  
+
+  let res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=${APIKEY}`)
+    // .then(response => response.json())
+    // .then(data => console.log(data.coord.lat));
+
+  let result = await res.json();
+  latitude = result.coord.lat;
+  console.log(latitude)
+
+  return lat;
 }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 0,
+    }
+  }
+
+  async componentDidMount() {
+    let lat = await GetTemp();
+    this.setState({lat: lat});
+
+  }
+
+  render () {
+    return (
+      <body>
+        <div className="top-part">
+          <div className="tempature">{this.state.lat}</div>
+          <div className="weather">sunny</div>
+        </div>
+        <div className="bottom-part"></div>
+      </body>
+    );
+  }
+};
 
 export default App;
